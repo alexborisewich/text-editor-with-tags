6bd9fafc5c9bfd908412eabf664c4a7bdf20720e
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-12-02 18:01:34 +0300

    feat: add ErrorPage and NotFoundPage lazy loading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { ErrorPage, Layout, NotesPage, NotFoundPage, TagsPage, WelcomePage } from 'components';
+import { Layout, NotesPage, TagsPage, WelcomePage } from 'components';
 import { ROUTER_PATHS } from 'settings';
+
+const NotFoundPage = React.lazy(() => import('components/pages/NotFoundPage'));
+const ErrorPage = React.lazy(() => import('components/pages/ErrorPage'));
 
 const App: React.FC = () => (
   <BrowserRouter basename={ROUTER_PATHS.base}>
@@ -12,8 +15,22 @@ const App: React.FC = () => (
         <Route path={ROUTER_PATHS.notes} element={<NotesPage />} />
         <Route path={ROUTER_PATHS.tags} element={<TagsPage />} />
       </Route>
-      <Route path={ROUTER_PATHS.notFound} element={<NotFoundPage />} />
-      <Route path={ROUTER_PATHS.error} element={<ErrorPage />} />
+      <Route
+        path={ROUTER_PATHS.notFound}
+        element={
+          <React.Suspense fallback='Loading'>
+            <NotFoundPage />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path={ROUTER_PATHS.error}
+        element={
+          <React.Suspense fallback='Loading'>
+            <ErrorPage />
+          </React.Suspense>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
