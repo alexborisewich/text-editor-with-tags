8c3ae0e724c2f87eb322c03f06fa78e746d952cb
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-12-04 19:56:49 +0300

    fix: fix bag with dispathing same tags when user add note first

--- src/utils/getTagsFromNote.ts
+++ src/utils/getTagsFromNote.ts
@@ -1,9 +1,13 @@
 export const getUniqueTagsFromNote = (note: string, tagsArr: string[]) => {
   if (note.includes('#') && !note.endsWith('#') && note.endsWith(' ')) {
-    return note
-      .toLowerCase()
-      .split(/(#[a-z\d-]+)/gi)
-      .filter((item) => item.startsWith('#') && item.length > 1 && !tagsArr.includes(item));
+    return [
+      ...new Set(
+        note
+          .toLowerCase()
+          .split(/(#[a-z\d-]+)/gi)
+          .filter((item) => item.startsWith('#') && item.length > 1 && !tagsArr.includes(item))
+      ),
+    ];
   }
   return [];
 };
